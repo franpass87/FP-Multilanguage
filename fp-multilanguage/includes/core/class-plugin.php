df2c9d71977f3734474976fa690fd5b933c8e1b5
franpass87 <francesco.passeri@gmail.com>
2025-10-14 19:27:29 +0200

    added error handling for the plugin activation

--- fp-multilanguage/includes/core/class-plugin.php
+++ fp-multilanguage/includes/core/class-plugin.php
@@ -312,44 +312,12 @@ class FPML_Plugin_Core {
 
 		// SKIP Health_Check - Causa errore 500
 		
-		// AGGIUNGO Auto_Detection e Auto_Translate - ESSENZIALI!
-		if ( class_exists( 'FPML_Auto_Detection' ) ) {
-			FPML_Auto_Detection::instance();
-			add_action( 'fpml_reindex_post_type', array( $this, 'reindex_post_type' ), 10, 1 );
-			add_action( 'fpml_reindex_taxonomy', array( $this, 'reindex_taxonomy' ), 10, 1 );
-		}
-
+		// TEST: Solo Auto_Translate (senza Auto_Detection)
 		if ( class_exists( 'FPML_Auto_Translate' ) ) {
 			FPML_Auto_Translate::instance();
 		}
 		
-		// Classi CORE per funzionamento base (non in assisted mode)
-		if ( ! $this->assisted_mode ) {
-			FPML_Rewrites::instance();
-			FPML_Language::instance();
-			FPML_Content_Diff::instance();
-			FPML_Processor::instance();
-			FPML_Menu_Sync::instance();
-			FPML_Media_Front::instance();
-			FPML_SEO::instance();
-		}
-
-		if ( class_exists( 'FPML_REST_Admin' ) ) {
-			FPML_REST_Admin::instance();
-		}
-
-		if ( is_admin() ) {
-			new FPML_Admin();
-		}
-
-		// Hook per gestione save_post, ecc.
-		if ( ! $this->assisted_mode ) {
-			add_action( 'save_post', array( $this, 'handle_save_post' ), 20, 3 );
-			add_action( 'created_term', array( $this, 'handle_created_term' ), 10, 3 );
-			add_action( 'edited_term', array( $this, 'handle_edited_term' ), 10, 3 );
-			add_action( 'before_delete_post', array( $this, 'handle_delete_post' ), 10, 1 );
-			add_action( 'delete_term', array( $this, 'handle_delete_term' ), 10, 3 );
-		}
+		// STOP QUI per test
 	}
 
 	/**
