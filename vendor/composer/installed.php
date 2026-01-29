de65623a2eaec6df8d6b9eacba165e7e8dd55bc8
franpass87 <francesco.passeri@gmail.com>
2026-01-29 15:14:53 +0100

    chore: include vendor in repo for Git Updater compatibility

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'francescopasseri/fp-multilanguage',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4cfd7f09c3e31ecdd823ca9850bfddcd05ca4cc9',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'francescopasseri/fp-multilanguage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4cfd7f09c3e31ecdd823ca9850bfddcd05ca4cc9',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
